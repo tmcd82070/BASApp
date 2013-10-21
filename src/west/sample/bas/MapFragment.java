42e44c5bcabd8adba0a125ede2d76e4e3b362ff0
MegB <megb@no.mail>
2013-10-21 11:12:26 -0600

    Recovered skeleton
    Initial layout of the BAS app

---
+++ src/west/sample/bas/MapFragment.java
@@ -0,0 +1,10 @@
+package west.sample.bas;
+
+import android.os.Bundle; 
+import android.support.v4.app.Fragment; 
+import android.view.*; 
+public class MapFragment extends Fragment { 
+	public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup, Bundle savedInstanceState) { 
+		return inflater.inflate(R.layout.fragment_map, viewGroup, false); 
+	} 
+}
